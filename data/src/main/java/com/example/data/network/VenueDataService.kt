e4ee18363062493747ad15d67d908f0c170ac702
Hossein <hosseingheisary1365@gmail.com>
2020-05-20 21:00:54 +0430

    Implement venue retrofit service

---
+++ data/src/main/java/com/example/data/network/VenueDataService.kt
@@ -0,0 +1,16 @@
+package com.example.data.network
+
+import com.example.data.entity.model.remote.VenuesResponse
+import io.reactivex.Flowable
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.QueryMap
+
+interface VenueDataService {
+
+    @GET("/v2/venues/explore")
+    fun exploreVenues(
+        @QueryMap queries: Map<String, String>,
+        @Query("radius") radius: Long = 1000
+    ): Flowable<VenuesResponse>
+}
