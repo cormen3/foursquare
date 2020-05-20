9b5e68ab8c1472802f1c362b4aee1ccbd49922f5
Hossein <hosseingheisary1365@gmail.com>
2020-05-20 20:47:27 +0430

    Implement venue viewHolder

--- presentation/src/main/java/com/example/presentation/ui/venue/fragments/list/view/adapter/VenuesViewHolder.kt
+++ presentation/src/main/java/com/example/presentation/ui/venue/fragments/list/view/adapter/VenuesViewHolder.kt
@@ -3,16 +3,18 @@ package com.example.presentation.ui.venue.fragments.list.view.adapter
 import android.view.View
 import com.example.domain.entity.venue.VenueObject
 import com.example.presentation.base.adapter.BaseViewHolder
+import com.example.presentation.base.adapter.ViewTypeHolder
 import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_venue.view.*
 
 open class VenuesViewHolder(override val containerView: View) :
     BaseViewHolder<VenueObject>(containerView),
     LayoutContainer {
 
+    override fun getType(): Int = ViewTypeHolder.VENUE_VIEW
+
     override fun bind(data: VenueObject?) {
+        itemView.itemVenueTitle.text = data?.name
     }
 
-    override fun getType(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 }
