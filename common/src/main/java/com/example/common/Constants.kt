e45a494d53e4527729e8ac0288ef0c1e7a6faa71
Hossein <hosseingheisary1365@gmail.com>
2020-05-20 20:58:53 +0430

    Implement permission constants

---
+++ common/src/main/java/com/example/common/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.common
+
+object Constants {
+    const val OBJECT = "object"
+    const val LIST = "list"
+
+    const val RC_APP_SETTING = 2003
+    const val RC_PERMISSION_LOCATION = 2001
+    const val RC_CHECK_SETTINGS = 1003
+}
