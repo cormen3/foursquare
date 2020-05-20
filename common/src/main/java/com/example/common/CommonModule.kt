c3b6a0ad2a70ac94c12f31c25e679154564eb074
Hossein <hosseingheisary1365@gmail.com>
2020-05-20 20:41:03 +0430

    Implement Preferences provider

--- common/src/main/java/com/example/common/CommonModule.kt
+++ common/src/main/java/com/example/common/CommonModule.kt
@@ -1,7 +1,23 @@
 package com.example.common
 
+import android.app.Application
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+import com.example.common.preferences.PreferencesModule
 import dagger.Module
+import dagger.Provides
 
-@Module
+@Module(
+    includes = [
+        PreferencesModule::class]
+)
 abstract class CommonModule {
+    @Module
+    companion object {
+        @JvmStatic
+        @Provides
+        fun sharedPreferences(application: Application): SharedPreferences {
+            return PreferenceManager.getDefaultSharedPreferences(application)
+        }
+    }
 }
