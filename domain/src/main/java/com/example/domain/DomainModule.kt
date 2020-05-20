452709a1c78476f6f600b756c7b27e6380c03805
Hossein <hosseingheisary1365@gmail.com>
2020-05-20 21:02:27 +0430

    Add transformers into domain module

--- domain/src/main/java/com/example/domain/DomainModule.kt
+++ domain/src/main/java/com/example/domain/DomainModule.kt
@@ -1,8 +1,31 @@
 package com.example.domain
 
+import com.example.domain.entity.venue.VenueObject
+import com.example.domain.entity.venue.VenuesObject
+import com.example.domain.transformer.*
+import dagger.Binds
 import dagger.Module
 
 @Suppress("unused")
 @Module
 abstract class DomainModule {
+    @Binds
+    abstract fun completableTransformer(
+        transformer: AsyncCTransformer
+    ): CTransformer
+
+    @Binds
+    abstract fun intSTransformer(
+        transformer: AsyncSTransformer<Boolean>
+    ): STransformer<Boolean>
+
+    @Binds
+    abstract fun venuesTransformer(
+        transformer: AsyncSTransformer<List<VenueObject>>
+    ): STransformer<List<VenueObject>>
+
+    @Binds
+    abstract fun venueFTransformer(
+        transformer: AsyncFTransformer<VenuesObject>
+    ): FTransformer<VenuesObject>
 }
