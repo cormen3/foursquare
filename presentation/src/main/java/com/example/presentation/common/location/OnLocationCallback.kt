f71a51a25a8e418da76d7d623038950f18c7b9c5
Hossein <hosseingheisary1365@gmail.com>
2020-05-21 15:38:38 +0430

    Remove on start locating function from location callback

--- presentation/src/main/java/com/example/presentation/common/location/OnLocationCallback.kt
+++ presentation/src/main/java/com/example/presentation/common/location/OnLocationCallback.kt
@@ -3,6 +3,5 @@ package com.example.presentation.common.location
 import android.location.Location
 
 interface OnLocationCallback {
-    fun onStartLocating()
     fun onNewLocation(location: Location?)
 }
