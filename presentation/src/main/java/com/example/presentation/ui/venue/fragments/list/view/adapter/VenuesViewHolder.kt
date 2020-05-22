28287e15ae348672303f4efa058c1a1d4864a3a9
Hossein <hosseingheisary1365@gmail.com>
2020-05-22 14:08:51 +0430

    Change venue list design

--- presentation/src/main/java/com/example/presentation/ui/venue/fragments/list/view/adapter/VenuesViewHolder.kt
+++ presentation/src/main/java/com/example/presentation/ui/venue/fragments/list/view/adapter/VenuesViewHolder.kt
@@ -2,6 +2,7 @@ package com.example.presentation.ui.venue.fragments.list.view.adapter
 
 import android.view.View
 import com.example.domain.entity.venue.VenueObject
+import com.example.presentation.R
 import com.example.presentation.base.adapter.BaseViewHolder
 import com.example.presentation.base.adapter.ViewTypeHolder
 import kotlinx.android.extensions.LayoutContainer
@@ -15,6 +16,11 @@ open class VenuesViewHolder(override val containerView: View) :
 
     override fun bind(data: VenueObject?) {
         itemView.itemVenueTitle.text = data?.name
+        itemView.itemVenueDistance.text = data?.location?.distance.toString().plus(
+            containerView.context.getString(R.string.meters)
+        )
+        itemView.itemVenueAddress.text = data?.location?.address
+        itemView.itemVenueFormattedAddress.text =
+            data?.location?.formattedAddress?.joinToString(",")
     }
-
 }
