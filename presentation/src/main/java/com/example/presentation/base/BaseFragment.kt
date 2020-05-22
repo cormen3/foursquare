92aa200069dcdbd3a002715695f843b52a674249
Hossein <hosseingheisary1365@gmail.com>
2020-05-22 06:51:10 +0430

    Fix error message in base view model

--- presentation/src/main/java/com/example/presentation/base/BaseFragment.kt
+++ presentation/src/main/java/com/example/presentation/base/BaseFragment.kt
@@ -2,7 +2,9 @@ package com.example.presentation.base
 
 import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.annotation.StringRes
+import com.example.presentation.common.connection.ConnectionManager
 import com.example.presentation.common.extension.toast
 import dagger.android.support.AndroidSupportInjection
 import dagger.android.support.DaggerFragment
@@ -12,6 +14,9 @@ abstract class BaseFragment : DaggerFragment(), MessagePresenter {
 
     @Inject
     protected lateinit var activityContext: Context
+
+    @Inject
+    lateinit var connectionManager: ConnectionManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +32,6 @@ abstract class BaseFragment : DaggerFragment(), MessagePresenter {
     }
 
     override fun showMessage(message: String) {
-        activity?.applicationContext?.toast(message)
+        activity?.applicationContext?.toast(message, Toast.LENGTH_SHORT)
     }
 }
